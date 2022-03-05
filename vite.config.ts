16d35d84aa294bd4de33d7f11208e262806bbcd7
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-04 22:19:44 -0300

    chore: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vitest/config'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@/': path.resolve('src'),
+      '@/tests/': path.resolve('tests')
+    }
+  },
   test: {
     include: ['tests/**/*.spec.ts'],
     globals: true
