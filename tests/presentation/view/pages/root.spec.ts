92ab94207711b3bd9414225f9a4c15e6517d832c
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-06 12:05:06 -0300

    test: add more tests for root page element replacement

--- tests/presentation/view/pages/root.spec.ts
+++ tests/presentation/view/pages/root.spec.ts
@@ -26,4 +26,27 @@ describe('RootPage', () => {
     expect(routerSpy.onNavigate).toHaveBeenCalledWith(expect.any(Function))
     expect(routerSpy.matchRoute).toHaveBeenCalledWith()
   })
+
+  it('should call replaceChild if RootPage has a child element', () => {
+    const { sut, routerSpy } = makeSut()
+    const span = document.createElement('span')
+    const replaceChildSpy = vi.spyOn(sut.root, 'replaceChild')
+    sut.root.append(span)
+
+    window.dispatchEvent(new Event('load'))
+
+    expect(replaceChildSpy).toHaveBeenCalledWith(
+      routerSpy.matchRoute.mock.results[0].value,
+      span
+    )
+  })
+
+  it('should call appendChild if RootPage has no child element', () => {
+    const { sut, routerSpy } = makeSut()
+    const appendChildSpy = vi.spyOn(sut.root, 'appendChild')
+
+    window.dispatchEvent(new Event('load'))
+
+    expect(appendChildSpy).toHaveBeenCalledWith(routerSpy.matchRoute.mock.results[0].value)
+  })
 })
