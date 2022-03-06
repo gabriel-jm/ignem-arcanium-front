01e41c3a7fbf98309f2570f889dca3b2dec74f0c
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-06 19:38:03 -0300

    test: ensure NotFoundPage button calls router correctly

---
+++ tests/presentation/view/pages/not-found.spec.ts
@@ -0,0 +1,24 @@
+import { NotFoundPage } from '@/presentation/view'
+import { router } from 'lithen-router'
+
+function makeSut() {
+  const sut = new NotFoundPage()
+  const routerSpy = {
+    goTo: vi.spyOn(router, 'goTo')
+  }
+
+  return {
+    sut,
+    routerSpy
+  }
+}
+
+describe('NotFoundPage', () => {
+  it('should call router.goTo on button click', () => {
+    const { sut, routerSpy } = makeSut()
+
+    sut.select('button')!.dispatchEvent(new Event('click'))
+
+    expect(routerSpy.goTo).toHaveBeenCalledWith('/')
+  })
+})
