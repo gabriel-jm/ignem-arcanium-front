c592c4b91aca5a0b728b5b8a97d0e12236ceef6d
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-03-07 15:24:00 -0300

    feat: create menu icon component

---
+++ src/presentation/view/components/menu-icon.ts
@@ -0,0 +1,43 @@
+import { IgnemElement } from '@/presentation/view'
+import { css, html } from 'lithen-tag-functions'
+
+export class MenuIcon extends IgnemElement {
+  constructor() {
+    super()
+    this.applyRender()
+  }
+
+  styling() {
+    return css`
+      :host {
+        width: 26px;
+        height: 26px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    `
+  }
+
+  render() {
+    return html`
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <line x1="3" y1="12" x2="21" y2="12"></line>
+        <line x1="3" y1="6" x2="21" y2="6"></line>
+        <line x1="3" y1="18" x2="21" y2="18"></line>
+      </svg>
+    `
+  }
+}
+
+customElements.define('ignem-menu-icon', MenuIcon)
--- src/presentation/view/index.ts
+++ src/presentation/view/index.ts
@@ -1,3 +1,4 @@
 export * from './ignem-element'
+export * from './components'
 export * from './pages'
 export * from './styles/shared-styles'
