9158ee65cd5209ea354054b3fa48d1ab2beee37f
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-03-11 14:56:04 -0300

    feat: first attempt to create ws connection

--- src/presentation/view/pages/home.ts
+++ src/presentation/view/pages/home.ts
@@ -1,8 +1,17 @@
+import { CreateConnection } from '@/domain/use-cases'
 import { IgnemElement } from '@/presentation/view'
 import { router } from 'lithen-router'
 import { css, html } from 'lithen-tag-functions'
 
 export class HomePage extends IgnemElement {
+  #createConnection: CreateConnection
+  
+  constructor(createConnection: CreateConnection) {
+    super()
+    this.#createConnection = createConnection
+    this.applyRender()
+  }
+
   styling() {
     return css`
       :host {
@@ -15,9 +24,25 @@ export class HomePage extends IgnemElement {
   }
 
   render() {
+    const onClick = async () => {
+      console.log('rodou')
+      
+      try {
+        const connectionId = await this.#createConnection.create()
+
+        console.log({ connectionId })
+
+        if(connectionId) {
+          router.goTo('/torches')
+        }
+      } catch (error) {
+        console.log('usecase error', error)
+      }
+    }
+
     return html`
       <h1>Home</h1>
-      <button class="btn" on-click=${() => router.goTo('/torches')}>
+      <button class="btn" on-click=${onClick}>
         Login
       </button>
     `
