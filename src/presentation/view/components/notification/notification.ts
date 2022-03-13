ff75c2475d2fadb0e5f805937d471e6084b33ac3
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-12 22:31:04 -0300

    feat: dynamic content and animation controls in notification

--- src/presentation/view/components/notification/notification.ts
+++ src/presentation/view/components/notification/notification.ts
@@ -1,24 +1,61 @@
 import { checkCircleIcon, IgnemElement } from '@/presentation/view'
-import { css, html } from 'lithen-tag-functions'
+import { html } from 'lithen-tag-functions'
+import { notificationStyles } from './notification-styles'
 
-export class IgnemNotification extends IgnemElement {
+interface IgnemNotificationProps {
+  title?: string
+  message?: string
+}
+
+export class IgnemNotification extends IgnemElement {  
+  #timeoutId: number | null = null
+
+  constructor(props: IgnemNotificationProps = {}) {
+    super()
+    props.title && this.setAttribute('title', props.title)
+    props.message && this.setAttribute('message', props.message)
+  }
+
+  hide = () => {
+    this.classList.add('hide')
+  }
+  
   styling() {
-    return css`
-      .notification-container {
-        display: block;
-        width: 200px;
-        border: 1px solid #111;
-        border-radius: 4px;
-        padding: 12px 16px;
-      }
-    `
+    return notificationStyles
   }
   
   render() {
+    this.on('animationend', (event: AnimationEventInit) => {
+      if (event.animationName === 'hide') {
+        this.remove()
+      }
+
+      if (event.animationName === 'show') {
+        const twoSecondsInMs = 2000
+        this.#timeoutId = Number(setTimeout(this.hide, twoSecondsInMs))
+      }
+    })
+
+    this.on('mouseenter', () => {
+      this.#timeoutId && clearTimeout(this.#timeoutId)
+      this.#timeoutId = null
+    })
+
+    this.on('mouseleave', () => {
+      !this.#timeoutId && setTimeout(this.hide, 800)
+    })
+
+    const title = this.getAttribute('title') ?? 'Unknown'
+    const message = this.getAttribute('message') ?? 'Unknown message'
+
     return html`
       <div class="notification-container">
         ${checkCircleIcon()}
-        <p>Example text</p>
+        <div class="notification-content">
+          <h4>${title}</h4>
+          <p>${message}</p>
+        </div>
+        <span on-click=${() => this.hide()}>&times;</span>
       </div>
     `
   }
