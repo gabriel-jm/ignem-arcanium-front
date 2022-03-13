f160900c04379c87db77772c83db950a809991ee
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-12 22:29:57 -0300

    feat: add animation to checked circle icon

--- src/presentation/view/components/check-circle-icon.ts
+++ src/presentation/view/components/check-circle-icon.ts
@@ -1,9 +1,40 @@
 import './wrapper'
-import { html } from 'lithen-tag-functions'
+import { css, html } from 'lithen-tag-functions'
 
 export function checkCircleIcon() {
+  const styles = css`
+    :host {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+    
+    svg path {
+      stroke-dasharray: 100;
+      stroke-dashoffset: 100;
+      animation: dash 800ms ease-out 1s forwards;
+    }
+
+    svg polyline {
+      stroke-dasharray: 100;
+      stroke-dashoffset: 100;
+      animation: dash 900ms ease-out 1.5s forwards;
+    }
+
+    @keyframes dash {
+      to {
+        stroke-dashoffset: 0;
+      }
+    }
+  `
+
   return html`
-    <ignem-wrapper>
+    <ignem-wrapper css="${styles}">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -16,7 +47,7 @@ export function checkCircleIcon() {
         stroke-linejoin="round"
       >
         <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
-        <polyline points="22 4 12 14.01 9 11.01"></polyline>
+        <polyline points="8 11 12 15 22 4"></polyline>
       </svg>
     </ignem-wrapper>
   `
