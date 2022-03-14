aae5fc80ec136980b4ceb28ba63113ce51cfe333
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-13 21:05:21 -0300

    feat: create alert icon

--- src/presentation/view/components/index.ts
+++ src/presentation/view/components/index.ts
@@ -1,5 +1,6 @@
 export * from './menu-icon'
 export * from './check-circle-icon'
+export * from './alert-icon'
 export * from './header'
 export * from './wrapper'
 export * from './torch/torch-registry'
