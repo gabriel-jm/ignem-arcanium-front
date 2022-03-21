7985d00d36676388a72dcc0feee9bcc8f81b0cab
Gabriel José <gabriel.josemelo@hotmail.com>
2022-03-20 23:30:24 -0300

    refactor: error handling in torch registry service

--- src/infra/services/torch-registry-service.ts
+++ src/infra/services/torch-registry-service.ts
@@ -10,49 +10,53 @@ export class TorchRegistryService implements FindAllTorchRegistriesService {
   
   findAll(): Promise<FindAllTorchRegistriesServiceResult[]> {
     return new Promise(async (resolve, reject) => {
-      const responseEvent = 'find-all-torch-registries-response'
+      try {
+        const responseEvent = 'find-all-torch-registries-response'
 
-      this.addMessageListenerOnceClient.once(responseEvent, payload => {
-        if (payload.statusCode === 200) {
-          resolve(payload.data as FindAllTorchRegistriesServiceResult[])
-        }
+        await this.addMessageListenerOnceClient.once(responseEvent, payload => {
+          if (payload.statusCode === 200) {
+            resolve(payload.data as FindAllTorchRegistriesServiceResult[])
+          }
 
-        reject(new ServiceError(payload))
-      })
-        .then(
-          async () => await this.sendMessageClient.send({
-            event: 'find-all-torch-registries'
-          }),
-          reject
-        )
+          reject(new ServiceError(payload))
+        })
+
+        await this.sendMessageClient.send({
+          event: 'find-all-torch-registries'
+        })
+      } catch (error) {
+        reject(error)
+      }
     })
   }
 
   async create(params: CreateTorchRegistryServiceParams) {
-    const promise = new Promise(async (resolve, reject) => {
-      await this.addMessageListenerOnceClient.once(
-        'create-torch-registry-response',
-        payload => {
-          console.log(payload)
-
-          if (payload.statusCode === 201) {
-            return resolve(payload.data)
+    return new Promise(async (resolve, reject) => {
+      try {
+        await this.addMessageListenerOnceClient.once(
+          'create-torch-registry-response',
+          payload => {
+            console.log(payload)
+  
+            if (payload.statusCode === 201) {
+              return resolve(payload.data)
+            }
+  
+            reject(new ServiceError(payload))
           }
-
-          reject(new ServiceError(payload))
-        }
-      )
-    })
-
-    await this.sendMessageClient.send({
-      event: 'create-torch-registry',
-      data: {
-        characterName: params.characterName,
-        torchCount: params.torchCount,
-        torchCharge: params.torchCharge
+        )
+  
+        await this.sendMessageClient.send({
+          event: 'create-torch-registry',
+          data: {
+            characterName: params.characterName,
+            torchCount: params.torchCount,
+            torchCharge: params.torchCharge
+          }
+        })
+      } catch(error) {
+        reject(error)
       }
     })
-
-    return promise
   }
 }
