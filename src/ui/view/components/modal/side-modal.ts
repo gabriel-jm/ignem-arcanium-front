c558ed897702fb426c653d5cb372281417161d6d
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-03-24 17:50:51 -0300

    feat: move side modal logic to its own component

---
+++ src/ui/view/components/modal/side-modal.ts
@@ -0,0 +1,121 @@
+import { containerStyles, IgnemElement } from '@/ui/view'
+import { css, html } from 'lithen-tag-functions'
+
+export interface IgnemSideModalElement extends IgnemElement {
+  open(): void
+  close(): void
+}
+
+export class IgnemSideModal extends IgnemElement {
+  open() {
+    this.classList.add('open')
+  }
+
+  close() {
+    this.classList.add('close')
+  }
+
+  styling() {
+    return css`
+      ${containerStyles}
+
+      :host {
+        display: none;
+        opacity: 0;
+        background-color: #0007;
+        z-index: 2;
+        position: absolute;
+        overflow: hidden;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        transition: all 400ms ease-in-out;
+      }
+
+      :host(.open) {
+        display: block;
+        opacity: 1;
+      }
+
+      :host(.close) {
+        animation: fade-out 400ms ease-in-out;
+      }
+
+      .container {
+        height: 100%;
+        padding: 10px;
+      }
+
+      .side-modal-container {
+        max-width: 400px;
+        height: 100%;
+        margin-left: auto;
+        background-color: var(--body-bg-color);
+        box-sizing: border-box;
+        border: 1px solid #3f3f3f;
+        border-radius: 4px;
+        padding: 10px 14px;
+        animation: slide-left 600ms ease-in-out;
+      }
+
+      :host(.close) .side-modal-container {
+        animation-name: slide-right;
+      }
+
+      @keyframes slide-left {
+        from {
+          transform: translateX(100%);
+          opacity: 0;
+        }
+
+        to {
+          transform: translateX(0);
+          opacity: 1;
+        }
+      }
+
+      @keyframes slide-right {
+        to {
+          transform: translateX(100%);
+          opacity: 0;
+        }
+      }
+
+      @keyframes fade-out {
+        to {
+          opacity: 0;
+        }
+      }
+    `
+  }
+  
+  render() {
+    this.classList.add('open')
+
+    this.on('click', event => {
+      const containerElement = this.select('.container')
+      const [clickedElement] = event.composedPath()
+      
+      if (clickedElement === this || clickedElement === containerElement) {
+        this.close()
+      }
+    })
+
+    this.on('animationend', (event: AnimationEventInit) => {
+      if (event.animationName === 'fade-out') {
+        this.classList.remove('open', 'close')
+      }
+    })
+
+    return html`
+      <section class="container">
+        <div class="side-modal-container">
+          <slot></slot>
+        </div>
+      </section>
+    `
+  }
+}
+
+customElements.define('ignem-side-modal', IgnemSideModal)
--- src/ui/view/components/torch/torch-side-modal.ts
+++ src/ui/view/components/torch/torch-side-modal.ts
@@ -1,70 +1,30 @@
-import { containerStyles, IgnemElement, inputStyles } from '@/ui/view'
+import { IgnemElement, IgnemSideModalElement, inputStyles } from '@/ui/view'
 import { css, html } from 'lithen-tag-functions'
 
-export interface IgnemTorchSideModalElement extends IgnemElement {
-  open(): void
-}
+export interface IgnemTorchSideModalElement extends IgnemSideModalElement {}
 
 /**
  * @event -form-submit
  */
 export class IgnemTorchSideModal extends IgnemElement {
+  #sideModal?: IgnemSideModalElement
+
+  constructor() {
+    super()
+    this.#sideModal = this.select('ignem-side-modal')
+  }
+
   open() {
-    this.classList.add('open')
+    this.#sideModal?.open()
   }
 
   close() {
-    this.classList.add('close')
+    this.#sideModal?.close()
   }
 
   styling() {
     return css`
-      ${containerStyles}
       ${inputStyles}
-
-      :host {
-        display: none;
-        opacity: 0;
-        background-color: #0007;
-        z-index: 2;
-        position: absolute;
-        overflow: hidden;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        transition: all 400ms ease-in-out;
-      }
-
-      :host(.open) {
-        display: block;
-        opacity: 1;
-      }
-
-      :host(.close) {
-        animation: fade-out 400ms ease-in-out;
-      }
-
-      .container {
-        height: 100%;
-        padding: 10px;
-      }
-
-      .side-modal-container {
-        max-width: 400px;
-        height: 100%;
-        margin-left: auto;
-        background-color: var(--body-bg-color);
-        box-sizing: border-box;
-        border: 1px solid #3f3f3f;
-        border-radius: 4px;
-        padding: 10px 14px;
-        animation: slide-left 600ms ease-in-out;
-      }
-
-      :host(.close) .side-modal-container {
-        animation-name: slide-right;
-      }
 
       .modal-header {
         display: flex;
@@ -112,52 +72,10 @@ export class IgnemTorchSideModal extends IgnemElement {
       .btn {
         margin-top: 12px;
       }
-
-      @keyframes slide-left {
-        from {
-          transform: translateX(100%);
-          opacity: 0;
-        }
-
-        to {
-          transform: translateX(0);
-          opacity: 1;
-        }
-      }
-
-      @keyframes slide-right {
-        to {
-          transform: translateX(100%);
-          opacity: 0;
-        }
-      }
-
-      @keyframes fade-out {
-        to {
-          opacity: 0;
-        }
-      }
     `
   }
   
   render() {
-    this.classList.add('open')
-
-    this.on('click', event => {
-      const containerElement = this.select('.container')
-      const [clickedElement] = event.composedPath()
-      
-      if (clickedElement === this || clickedElement === containerElement) {
-        this.close()
-      }
-    })
-
-    this.on('animationend', (event: AnimationEventInit) => {
-      if (event.animationName === 'fade-out') {
-        this.classList.remove('open', 'close')
-      }
-    })
-
     const onInput: EventListener = event => {
       const input = event.target as HTMLInputElement
       input.value = input.value.replace(/\D+/g, '')
@@ -181,38 +99,36 @@ export class IgnemTorchSideModal extends IgnemElement {
     }
 
     return html`
-      <section class="container">
-        <div class="side-modal-container">
-          <header class="modal-header">
-            <h3 class="modal-title">Create Torch Registry</h3>
-            <button class="close-btn" on-click=${this.close.bind(this)}>
-              &times;
-            </button>
-          </header>
+      <ignem-side-modal>
+        <header class="modal-header">
+          <h3 class="modal-title">Create Torch Registry</h3>
+          <button class="close-btn" on-click=${this.close.bind(this)}>
+            &times;
+          </button>
+        </header>
 
-          <form on-submit=${onSubmit}>
-            <label>
-              <span>Character Name</span>
-              <input class="input" name="characterName" />
-              <span class="input-message"></span>
-            </label>
+        <form on-submit=${onSubmit}>
+          <label>
+            <span>Character Name</span>
+            <input class="input" name="characterName" />
+            <span class="input-message"></span>
+          </label>
 
-            <label>
-              <p>Torch Count</p>
-              <input class="input" name="torchCount" on-input=${onInput} />
-              <span class="input-message"></span>
-            </label>
+          <label>
+            <p>Torch Count</p>
+            <input class="input" name="torchCount" on-input=${onInput} />
+            <span class="input-message"></span>
+          </label>
 
-            <label>
-              <p>Torch Charge</p>
-              <input class="input" name="torchCharge" on-input=${onInput} />
-              <span class="input-message"></span>
-            </label>
+          <label>
+            <p>Torch Charge</p>
+            <input class="input" name="torchCharge" on-input=${onInput} />
+            <span class="input-message"></span>
+          </label>
 
-            <button class="btn">Submit</button>
-          </form>
-        </div>
-      </section>
+          <button class="btn">Submit</button>
+        </form>
+      </ignem-side-modal>
     `
   }
 }
