28c152d53599560b6db597a650b4dc9877dccd6a
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-04-06 17:50:51 -0300

    feat: create close icon component

---
+++ src/ui/view/components/icons/close-icon.ts
@@ -0,0 +1,22 @@
+import '@/ui/view/components/wrapper'
+import { iconsStyles } from '@/ui/view/styles'
+import { html } from 'lithen-tag-functions'
+
+export const closeIcon = () => html`
+  <ignem-wrapper class="close-icon" css="${iconsStyles}">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  </ignem-wrapper>
+`
