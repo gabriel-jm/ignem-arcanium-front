cbeda0062156c186210c65f387bbdae19daf49c5
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-04-08 09:55:28 -0300

    refactor: move button styles from shared to its own file

--- src/ui/view/styles/shared-styles.ts
+++ src/ui/view/styles/shared-styles.ts
@@ -9,27 +9,4 @@ export const sharedStyles = () => defineSharedStyles(css`
     box-sizing: border-box;
     list-style: none;
   }
-
-  .btn {
-    font-family: 'Nunito', sans-serif;
-    background-color: var(--btn-bg-color);
-    cursor: pointer;
-    color: var(--font-color);
-    padding: 10px 14px;
-    border-radius: 4px;
-    border: 0;
-    transition: background-color 150ms;
-  }
-
-  .btn:hover {
-    background-color: var(--btn-hover-bg-color);
-  }
-
-  .btn:focus {
-    outline: 2px solid var(--btn-focus-outline-color);
-  }
-
-  .btn:active {
-    background-color: var(--btn-active-bg-color);
-  }
 `)
