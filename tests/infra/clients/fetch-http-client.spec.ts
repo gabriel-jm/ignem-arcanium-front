0ba230bed2ce9ecd3ef86de481f7b4b0e22df9df
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-04-26 18:29:55 -0300

    test: ensure FetchHTTPClient returns the correct response

--- tests/infra/clients/fetch-http-client.spec.ts
+++ tests/infra/clients/fetch-http-client.spec.ts
@@ -3,7 +3,10 @@ import { FetchHTTPClient } from '@/infra/clients'
 function makeSut() {
   const fetchSpy = vi.spyOn(window, 'fetch')
   fetchSpy.mockImplementation(() => Promise.resolve({
-    json() {}
+    status: 200,
+    json() {
+      return {}
+    }
   } as Response))
   const fakeURL = 'http://any-url.com'
   const sut = new FetchHTTPClient(fakeURL)
@@ -20,17 +23,40 @@ describe('FetchHTTPClient', () => {
     const { sut, fakeURL, fetchSpy } = makeSut()
 
     await sut.request({
+      method: 'post',
+      path: '/',
+      headers: {
+        auth: 'any_token'
+      },
+      body: {
+        any: 'data'
+      }
+    })
+
+    expect(fetchSpy).toHaveBeenCalledWith(`${fakeURL}/`, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        auth: 'any_token'
+      },
+      body: JSON.stringify({
+        any: 'data'
+      })
+    })
+  })
+
+  it('should parse return the status code and body on success', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.request({
       method: 'get',
       path: '/'
     })
 
-    expect(fetchSpy).toHaveBeenCalledWith(`${fakeURL}/`, {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: null
+    expect(response).toEqual({
+      statusCode: 200,
+      body: {}
     })
   })
 })
