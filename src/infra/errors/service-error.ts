be59a7a7eee1301e59470fd4bb053cd1e766a40c
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-05 19:24:26 -0300

    feat(WIP): create login on account service

--- src/infra/errors/service-error.ts
+++ src/infra/errors/service-error.ts
@@ -1,7 +1,8 @@
 export class ServiceError extends Error {
   constructor(
     public readonly data: unknown,
-    public readonly message = 'Service response error'
+    public readonly message = 'Service response error',
+    public readonly skipNotification = false
   ) {
     super(message)
   }
--- src/infra/services/account-service.ts
+++ src/infra/services/account-service.ts
@@ -1,4 +1,7 @@
 import {
+  AccountLoginService,
+  AccountLoginServiceParams,
+  AccountLoginServiceResult,
   CreateAccountService,
   CreateAccountServiceParams,
   CreateAccountServiceResult
@@ -6,8 +9,29 @@ import {
 import { ServiceError } from '@/infra/errors'
 import { HTTPClient } from '@/infra/protocols'
 
-export class AccountService implements CreateAccountService {
+export class AccountService implements CreateAccountService, AccountLoginService {
   constructor(private readonly httpClient: HTTPClient) {}
+
+  async login(params: AccountLoginServiceParams) {
+    const response = await this.httpClient.request<AccountLoginServiceResult>({
+      method: 'post',
+      path: '/login',
+      body: {
+        email: params.email,
+        password: params.password
+      }
+    })
+
+    if (response.statusCode >= 400) {
+      throw new ServiceError(
+        response.body,
+        'Error on login',
+        true
+      )
+    }
+
+    return response.body
+  }
   
   async create(params: CreateAccountServiceParams): Promise<CreateAccountServiceResult> {
     const response = await this.httpClient.request<CreateAccountServiceResult>({
