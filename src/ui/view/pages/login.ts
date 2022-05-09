1093614ffa6968b7bd68b21855477c759a1ec9b7
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-09 18:13:43 -0300

    feat: change login page to have a form

--- src/ui/view/pages/login.ts
+++ src/ui/view/pages/login.ts
@@ -1,19 +1,77 @@
-import '../components/wrapper'
 import { router } from 'lithen-router'
-import { html } from 'lithen-tag-functions'
+import { css, html } from 'lithen-tag-functions'
 import { IgnemElement } from '@/ui/view/ignem-element'
+import { ignemInput } from '@/ui/view/components'
+import { containerStyles, buttonStyles, inputStyles } from '@/ui/view/styles'
 
 export class IgnemLoginPage extends IgnemElement {
+  styling() {
+    return css`
+      ${[containerStyles, buttonStyles, inputStyles]}
+
+      .container {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .login-form {
+        max-width: 320px;
+        padding: 20px;
+      }
+
+      .form-title {
+        font-size: 2.4rem;
+        text-align: center;
+        margin-bottom: 45px;
+      }
+
+      .login-form label {
+        display: block;
+        margin: 8px 0;
+      }
+
+      .btn {
+        width: 100%;
+        margin-top: 16px;
+      }
+
+      .btn:disabled {
+        pointer-events: none;
+        filter: brightness(0.6);
+      }
+    `
+  }
+
   render() {
-    function handleClick() {
+    function handleSubmit() {
       router.goTo('/create-account')
     }
   
     return html`
-      <h1>Login</h1>
-      <button on-click=${handleClick}>
-        Create Account
-      </button>
+      <section class="container">
+        <form is="ignem-form" class="login-form" on-submit=${handleSubmit}>
+          <h1 class="form-title">Login</h1>
+
+          ${ignemInput({
+            label: 'E-Mail',
+            name: 'email',
+            className: 'input',
+            placeholder: 'E-Mail'
+          })}
+
+          ${ignemInput({
+            label: 'Password',
+            className: 'input',
+            name: 'password',
+            type: 'password',
+            placeholder: 'Password'
+          })}
+
+          <button class="btn">Create</button>
+        </form>
+      </section>
     `
   }
 }
