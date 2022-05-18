d0e1151e64dbc274cce86c0c85f0f2d2fa938ba3
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-18 15:55:26 -0300

    refactor: move domain files to separated folders

--- src/domain/protocols/use-cases/index.ts
+++ src/domain/protocols/use-cases/index.ts
@@ -1,7 +1,9 @@
-export * from './find-all-torch-registries'
-export * from './create-connection'
-export * from './create-torch-registry'
-export * from './update-torch-registry'
-
-export * from './account/create-account'
 export * from './account/account-login'
+export * from './account/create-account'
+
+export * from './connection/create-connection'
+
+export * from './torch-registry/find-all-torch-registries'
+export * from './torch-registry/create-torch-registry'
+export * from './torch-registry/update-torch-registry'
+
--- src/domain/use-cases/index.ts
+++ src/domain/use-cases/index.ts
@@ -1,7 +1,8 @@
-export * from './remote-find-all-torch-registries'
-export * from './remote-create-connection'
-export * from './remote-create-torch-registry'
-export * from './remote-update-torch-registry'
-
 export * from './account/remote-create-account'
 export * from './account/remote-account-login'
+
+export * from './connection/remote-create-connection'
+
+export * from './torch-registry/remote-find-all-torch-registries'
+export * from './torch-registry/remote-create-torch-registry'
+export * from './torch-registry/remote-update-torch-registry'
