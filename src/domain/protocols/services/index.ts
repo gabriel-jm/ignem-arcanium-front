1e74a8da2eae1294c8a55c469656b4613a53999c
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-05-20 10:36:46 -0300

    refactor: move protocols and presenters to its own folders

--- src/domain/protocols/services/index.ts
+++ src/domain/protocols/services/index.ts
@@ -1,8 +1,9 @@
-export * from './find-all-torch-registries-service'
-export * from './create-connection-service'
-export * from './create-torch-registry-service'
-export * from './update-torch-registry-service'
+export * from './connection/create-connection-service'
 
 export * from './account/create-account-service'
 export * from './account/account-login-service'
 export * from './account/verify-token-service'
+
+export * from './torch-registry/find-all-torch-registries-service'
+export * from './torch-registry/create-torch-registry-service'
+export * from './torch-registry/update-torch-registry-service'
--- src/presentation/presenters/index.ts
+++ src/presentation/presenters/index.ts
@@ -1,9 +1,10 @@
-export * from './create-connection-presenter'
-export * from './find-all-torch-registries-presenter'
-export * from './create-torch-registry-presenter'
-export * from './update-torch-registry-presenter'
+export * from './connection/create-connection-presenter'
 
 export * from './account/create-account-presenter'
 export * from './account/verify-token-presenter'
 
 export * from './generic/generic-presenter'
+
+export * from './torch-registry/find-all-torch-registries-presenter'
+export * from './torch-registry/create-torch-registry-presenter'
+export * from './torch-registry/update-torch-registry-presenter'
