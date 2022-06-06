c73e75ed23eec0e80e7efcce6aa43657c36fff18
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-06-06 17:22:25 -0300

    refactor: move presenters factories to separate folders

--- src/main/factories/presenters/index.ts
+++ src/main/factories/presenters/index.ts
@@ -1,7 +1,7 @@
-export * from './create-torch-registry-presenter-factory'
-export * from './find-all-torch-registries-presenter-factory'
-export * from './verify-token-presenter-factory'
-export * from './update-torch-registry-presenter-factory'
+export * from './torch-registry/create-torch-registry-presenter-factory'
+export * from './torch-registry/find-all-torch-registries-presenter-factory'
+export * from './torch-registry/update-torch-registry-presenter-factory'
 
+export * from './account/verify-token-presenter-factory'
 export * from './account/check-token-exists-presenter-factory'
 export * from './account/login-presenter-factory'
