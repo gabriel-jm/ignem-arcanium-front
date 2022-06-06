0ff52193c32a7f3d00db08db8719ef89475668eb
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-06-06 11:58:49 -0300

    feat: create find all characters usecase protocol

---
+++ src/domain/protocols/use-cases/character/find-all-characters.ts
@@ -0,0 +1,19 @@
+export interface FindAllCharactersResult {
+  id: string
+  name: string
+  icon: string
+  level: number
+  gold: number
+  hp: number
+  mp: number
+  strength: number
+  dexterity: number
+  constitution: number
+  intelligence: number
+  wisdom: number
+  charism: number
+}
+
+export interface FindAllCharacters {
+  findAll(): Promise<FindAllCharactersResult[]>
+}
--- src/domain/protocols/use-cases/index.ts
+++ src/domain/protocols/use-cases/index.ts
@@ -6,7 +6,8 @@ export * from './account/check-token-exists'
 
 export * from './connection/create-connection'
 
+export * from './character/find-all-characters'
+
 export * from './torch-registry/find-all-torch-registries'
 export * from './torch-registry/create-torch-registry'
 export * from './torch-registry/update-torch-registry'
-
