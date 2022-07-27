312b3aaae884649baba8ecd05bcae40dd6a176fe
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-07-27 11:37:46 -0300

    refactor: organize login page files

--- src/ui/view/pages/login/ignem-login-page.ts
+++ src/ui/view/pages/login/ignem-login-page.ts
@@ -1,16 +1,15 @@
 import { router } from 'lithen-router'
-import { css, html } from 'lithen-tag-functions'
+import { html } from 'lithen-tag-functions'
 import { IgnemElement } from '@/ui/view/ignem-element'
 import {
   IgnemFormElement,
   ignemInput,
   loadingIcon,
-  textBetweenDashes,
-  textBetweenDashesStyles
+  textBetweenDashes
 } from '@/ui/view/components'
-import { containerStyles, buttonStyles, inputStyles } from '@/ui/view/styles'
 import { Presenter } from '@/presentation/protocols'
 import { SuccessNotifier } from '@/ui/protocols'
+import { loginStyles } from '@/ui/view/pages/login/login-styles'
 
 let tokenChecked = false
 
@@ -52,83 +51,7 @@ export class IgnemLoginPage extends IgnemElement {
   }
 
   styling() {
-    const additionalStyles = [
-      containerStyles,
-      buttonStyles,
-      inputStyles,
-      textBetweenDashesStyles
-    ]
-
-    return css`
-      ${additionalStyles}
-
-      .container {
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .login-form {
-        max-width: 320px;
-        padding: 20px;
-      }
-
-      .form-title {
-        font-size: 2.4rem;
-        text-align: center;
-        margin-bottom: 45px;
-      }
-
-      .login-form label {
-        display: block;
-        margin: 8px 0;
-      }
-
-      .btn {
-        width: 100%;
-        margin-top: 16px;
-      }
-
-      .btn:disabled {
-        pointer-events: none;
-        filter: brightness(0.6);
-      }
-
-      .link {
-        display: block;
-        text-decoration: underline;
-        background-color: transparent;
-        border: 0;
-        font-size: 1.1rem;
-        font-weight: normal;
-        font-family: var(--font-family);
-        width: 100%;
-        text-align: center;
-        cursor: pointer;
-        color: var(--sub-font-color);
-      }
-
-      .loading {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        background-color: #0005;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .loading.hide {
-        display: none;
-      }
-
-      .loading .loading-icon {
-        width: 80px;
-      }
-    `
+    return loginStyles
   }
 
   render() {
---
+++ src/ui/view/pages/login/login-styles.ts
@@ -0,0 +1,81 @@
+import { textBetweenDashesStyles } from '@/ui/view/components'
+import { containerStyles, buttonStyles, inputStyles } from '@/ui/view/styles'
+import { css } from 'lithen-tag-functions'
+
+const additionalStyles = [
+  containerStyles,
+  buttonStyles,
+  inputStyles,
+  textBetweenDashesStyles
+]
+
+export const loginStyles = css`
+  ${additionalStyles}
+
+  .container {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .login-form {
+    max-width: 320px;
+    padding: 20px;
+  }
+
+  .form-title {
+    font-size: 2.4rem;
+    text-align: center;
+    margin-bottom: 45px;
+  }
+
+  .login-form label {
+    display: block;
+    margin: 8px 0;
+  }
+
+  .btn {
+    width: 100%;
+    margin-top: 16px;
+  }
+
+  .btn:disabled {
+    pointer-events: none;
+    filter: brightness(0.6);
+  }
+
+  .link {
+    display: block;
+    text-decoration: underline;
+    background-color: transparent;
+    border: 0;
+    font-size: 1.1rem;
+    font-weight: normal;
+    font-family: var(--font-family);
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+    color: var(--sub-font-color);
+  }
+
+  .loading {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: #0005;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .loading.hide {
+    display: none;
+  }
+
+  .loading .loading-icon {
+    width: 80px;
+  }
+`
