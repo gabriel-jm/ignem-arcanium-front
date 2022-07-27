4ea59f1133d71477c2834bd61c350a80ab67da6f
Gabriel Jose <gabriel.josemelo@hotmail.com>
2022-07-27 11:47:24 -0300

    refactor: organize root files

--- src/ui/view/pages/index.ts
+++ src/ui/view/pages/index.ts
@@ -1,4 +1,4 @@
-export * from './root'
+export * from './root/ignem-root'
 export * from './not-found/ignem-not-found-page'
 export * from './intro/ignem-introduction-page'
 export * from './torches/ignem-torches-page'
