66f8a654e666f2cad0eee5299f00e069d4fbf08e
Gabriel José <gabriel.josemelo@hotmail.com>
2022-07-31 21:35:01 -0300

    feat: create create character page component

---
+++ src/ui/view/pages/create-characters/ignem-create-character-page.ts
@@ -0,0 +1,7 @@
+import { IgnemElement } from '@/ui/view/ignem-element'
+
+export class IgnemCreateCharacterPage extends IgnemElement {
+
+}
+
+customElements.define('ignem-create-character-page', IgnemCreateCharacterPage)
