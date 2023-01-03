3df290df1c3470cbde7e63374a923cc5433f0aac
Gabriel José <gabriel.josemelo@hotmail.com>
2023-01-03 15:21:44 -0300

    feat: style attributes display

--- src/ui/view/pages/characters/modal/character-modal-styles.ts
+++ src/ui/view/pages/characters/modal/character-modal-styles.ts
@@ -1,5 +1,58 @@
+import { characterStatsStyles } from '@/ui/view/styles'
 import { css } from 'lithen-tag-functions'
 
 export const characterModalStyles = css`
-  
+  ${characterStatsStyles}
+
+  dialog {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    padding: 24px 8%;
+    border: 0;
+    background-color: #0007;
+    overflow-y: auto;
+    color: var(--font-color);
+  }
+
+  .modal-container {
+    background-color: var(--body-bg-color);
+    border-radius: 4px;
+    padding: 12px 16px;
+    box-shadow: 0 2px 8px 2px #0008;
+  }
+
+  .modal-header {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+  }
+
+  .modal-header figure {
+    width: 80px;
+  }
+
+  .modal-header h1 {
+    margin-bottom: 4px;
+  }
+
+  .primary-stats-container span {
+    margin-right: 16px;
+  }
+
+  .attributes-group {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 18px;
+  }
+
+  .attributes-group div {
+    display: flex;
+    gap: 0 16px;
+    flex-wrap: wrap;
+  }
 `
--- src/ui/view/pages/characters/modal/ignem-character-modal.ts
+++ src/ui/view/pages/characters/modal/ignem-character-modal.ts
@@ -1,7 +1,7 @@
 import { FindAllCharactersResult } from '@/domain/protocols/use-cases'
 import { IgnemElement } from '@/ui/view/ignem-element'
-import { characterStatsStyles } from '@/ui/view/styles'
-import { css, html } from 'lithen-tag-functions'
+import { characterModalStyles } from '@/ui/view/pages/characters/modal/character-modal-styles'
+import { html } from 'lithen-tag-functions'
 
 export type CharacterModalData = FindAllCharactersResult
 
@@ -11,7 +11,6 @@ export class IgnemCharacterModal extends IgnemElement {
   }
 
   open(data: CharacterModalData) {
-    console.log(data)
     this.dialog.show()
     this.#applyDialogContent(data)
   }
@@ -23,47 +22,7 @@ export class IgnemCharacterModal extends IgnemElement {
   }
 
   styling() {
-    return css`
-      ${characterStatsStyles}
-
-      dialog {
-        position: fixed;
-        top: 0;
-        left: 0;
-        width: 100vw;
-        height: 100vh;
-        padding: 24px 8%;
-        border: 0;
-        background-color: #0007;
-        overflow-y: auto;
-        color: var(--font-color);
-      }
-
-      .modal-container {
-        background-color: var(--body-bg-color);
-        border-radius: 4px;
-        padding: 12px 16px;
-        box-shadow: 0 2px 8px 2px #0008;
-      }
-
-      .modal-header {
-        display: flex;
-        align-items: center;
-        gap: 12px;
-      }
-
-      .modal-header figure {
-        width: 80px;
-      }
-
-      .modal-header h1 {
-        margin-bottom: 4px;
-      }
-
-      .primary-stats-container span {
-        margin-right: 16px;
-      }
-    `
+    return characterModalStyles
   }
 
   render() {
@@ -96,18 +55,20 @@ function characterModalContent(modal: IgnemCharacterModal, data: CharacterModalD
         </div>
       </header>
 
-      <div>
-        <span>HP ${data.hp}</span>
-        <span>MP ${data.mp}</span>
-      </div>
+      <div class="attributes-group">
+        <div>
+          <span>HP ${data.hp}</span>
+          <span>MP ${data.mp}</span>
+        </div>
 
-      <div>
-        <span>Strength ${data.strength}</span>
-        <span>Dexterity ${data.dexterity}</span>
-        <span>Constitution ${data.constitution}</span>
-        <span>Intelligence ${data.intelligence}</span>
-        <span>Wisdom ${data.wisdom}</span>
-        <span>Charisma ${data.charisma}</span>
+        <div>
+          <span>Strength ${data.strength}</span>
+          <span>Dexterity ${data.dexterity}</span>
+          <span>Constitution ${data.constitution}</span>
+          <span>Intelligence ${data.intelligence}</span>
+          <span>Wisdom ${data.wisdom}</span>
+          <span>Charisma ${data.charisma}</span>
+        </div>
       </div>
 
       <section>
