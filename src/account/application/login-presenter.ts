6c39235b9fd7b18386c0938d50ca8e81db4227eb
Gabriel Jose <gabriel.josemelo@hotmail.com>
2023-02-13 11:32:18 -0300

    refactor: move login request logic to login presenter

--- src/account/application/login-presenter.ts
+++ src/account/application/login-presenter.ts
@@ -1,26 +1,41 @@
-import { CacheStore } from '@/common/infra/protocols'
+import { CacheStore, HTTPClient } from '@/common/infra/protocols'
+import { HTTPServiceError } from '@/infra/errors'
 import { successResponse } from '@/presentation/helpers'
 import { Presenter } from '@/presentation/protocols'
 import { SetAccountStore } from '@/presentation/protocols/stores'
-import { AccountLoginService } from './protocols'
 
 interface LoginPresenterParams {
   email: string
   password: string
 }
 
+interface ServerLoginData {
+  name: string
+  token: string
+}
+
 export class LoginPresenter implements Presenter {
   constructor(
-    private readonly accountLoginService: AccountLoginService,
+    private readonly httpClient: HTTPClient,
     private readonly cacheStore: CacheStore,
     private readonly setAccountStore: SetAccountStore
   ) {}
   
-  async handle(data: LoginPresenterParams) {
-    const accessCredentials = await this.accountLoginService.login({
-      email: data.email,
-      password: data.password
+  async handle(params: LoginPresenterParams) {
+    const response = await this.httpClient.request<ServerLoginData>({
+      method: 'post',
+      path: '/login',
+      body: {
+        email: params.email,
+        password: params.password
+      }
     })
+
+    if (response.statusCode >= 400) {
+      throw new HTTPServiceError(response, 'Error on login')
+    }
+
+    const accessCredentials = response.body
 
     this.cacheStore.save('token', { token: accessCredentials.token })
 
